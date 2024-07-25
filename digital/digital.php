faccf278fa4025dc5d3e31c207ba098fa3778e0b
unknown <ksena.vilkova79@gmail.com>
2024-07-25 17:46:28 +0500

    Главная страница, автор, html\css digital

---
+++ digital/digital.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Art Porfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+
+    
+    <div class="header">
+        <li>
+            <a href="../index/index.php">HOME</a>
+            <a href="digital.php">DIGITAL</a>
+            <a href="../tradition/tradition.php">TRADITION</a>
+            <a href="../author/author.php">AUTHOR</a>
+        </li>
+    </div>
+
+    <div class="content-year">
+        <h1 class="text-year">2019 year</h1>
+    </div>
+
+    <div class="img-content">
+        <img src="img/1.jpg" alt="">
+        <img src="img/2.jpg" alt="">
+        <img src="img/3.jpg" alt="">
+        <img src="img/4.jpg" alt="">
+        <img src="img/5.jpg" alt="">
+    </div>
+
+    <div class="content-year">
+        <h1 class="text-year">2018 year</h1>
+    </div>
+
+    <div class="img-content">
+        <img src="img/1.jpg" alt="">
+        <img src="img/2.jpg" alt="">
+        <img src="img/3.jpg" alt="">
+        <img src="img/4.jpg" alt="">
+        <img src="img/5.jpg" alt="">
+    </div>
+
+</body>
+</html>
---
+++ index/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Art Porfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <div class="container">
+        <a href="index.php" class="title">HOLLSPAE</a>
+    </div>
+    
+    <div class="header">
+        <li>
+            <a href="index.php">HOME</a>
+            <a href="../digital/digital.php">DIGITAL</a>
+            <a href="../tradition/tradition.php">TRADITION</a>
+            <a href="../author/author.php">AUTHOR</a>
+        </li>
+    </div>
+
+    <div class="IndexIMG">
+        <img src="img/cat.jpg" alt="">
+    </div>
+   
+ 
+</body>
+</html>
